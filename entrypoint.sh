30ffed8f71bb78b7592083c9dcfb91ad0971320a
Izaak Lauer <8404559+izaaklauer@users.noreply.github.com>
2023-05-16 18:07:02 -0400

    Allowing configuring the DB name

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -z "${PG_CRON_DATABSE_NAME}" ]; then
+    PG_CRON_DATABSE_NAME='postgres'
+fi
+
+echo "using pg_cron database_name $PG_CRON_DATABSE_NAME"
+
+exec docker-entrypoint.sh -c cron.database_name=${PG_CRON_DATABSE_NAME} -c shared_preload_libraries=pg_cron
